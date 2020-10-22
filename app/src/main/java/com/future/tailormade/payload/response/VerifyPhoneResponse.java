8abbc1885c7af4fb1ecc250f07c1b9a8e8c8cd34
Syntia <53588149+syntialai@users.noreply.github.com>
2020-10-22 21:29:41 +0700

    AUTH-MODELS: Added login and verifyPhone payload

---
+++ app/src/main/java/com/future/tailormade/payload/response/VerifyPhoneResponse.java
@@ -0,0 +1,15 @@
+package com.future.tailormade.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyPhoneResponse {
+
+    private String verificationCode;
+}
