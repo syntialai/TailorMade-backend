9c82b5f9395d26acc0190976cef4b816449fbe36
syntialai <53588149+syntialai@users.noreply.github.com>
2020-10-22 23:49:31 +0700

    AUTH-SECURITY-CONFIG: Add ApiPath constants for auth

---
+++ app/src/main/java/com/future/tailormade/config/ApiPath.java
@@ -0,0 +1,11 @@
+package com.future.tailormade.config;
+
+public class ApiPath {
+
+    /**
+     * Auth API Path
+     */
+    public static final String AUTH_LOGIN = "/login";
+    public static final String AUTH_SIGNUP = "/sign-up";
+    public static final String AUTH_VERIFY_PHONE = "/verify-phone";
+}
