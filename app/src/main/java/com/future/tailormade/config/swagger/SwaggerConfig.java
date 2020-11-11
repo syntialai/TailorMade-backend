ac0bdbcf2f4d01b752209baa01739f9c6f407624
Syntia <53588149+syntialai@users.noreply.github.com>
2020-11-12 01:50:08 +0700

    AUTH-CONTROLLER: Added Swagger UI

---
+++ app/src/main/java/com/future/tailormade/config/swagger/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.future.tailormade.config.swagger;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+@Configuration
+@EnableSwagger2WebFlux
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.future.tailormade.controller"))
+                .build()
+                .genericModelSubstitutes(DeferredResult.class, ResponseEntity.class);
+    }
+}
