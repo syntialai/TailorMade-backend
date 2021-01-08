a5a8edeebfc36bbca384a4a27c87e2247e3b5b8b
syntialai <53588149+syntialai@users.noreply.github.com>
2021-01-09 00:25:23 +0700

    MANAGE_WISHLIST_API: Add command to get wishlists

--- app/src/main/java/com/future/tailormade/repository/WishlistRepository.java
+++ app/src/main/java/com/future/tailormade/repository/WishlistRepository.java
@@ -1,9 +1,20 @@
 package com.future.tailormade.repository;
 
 import com.future.tailormade.model.entity.wishlist.Wishlist;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface WishlistRepository extends ReactiveMongoRepository<Wishlist, String> {
+
+    Mono<Integer> countAllByUserId(String userId);
+
+    @Query("{ id: { $exists: true }}")
+    Flux<Wishlist> findAllByUserId(String userId, Pageable pageable);
+
+    Mono<Wishlist> findByUserIdAndId(String userId, String id);
 }
