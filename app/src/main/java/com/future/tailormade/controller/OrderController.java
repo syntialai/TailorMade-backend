bcc1d6652c24cbf0413f88c2fef39d67c9aedd8f
syntialai <53588149+syntialai@users.noreply.github.com>
2021-01-11 17:47:21 +0700

    MANAGE_ORDER_API: Add controller for get orders by user and tailor

--- app/src/main/java/com/future/tailormade/controller/OrderController.java
+++ app/src/main/java/com/future/tailormade/controller/OrderController.java
@@ -5,28 +5,85 @@ import com.blibli.oss.common.response.Response;
 import com.blibli.oss.common.response.ResponseHelper;
 import com.future.tailormade.command.order.AcceptTailorOrderCommand;
 import com.future.tailormade.command.order.GetTailorOrderByIdCommand;
+import com.future.tailormade.command.order.GetTailorOrdersCommand;
 import com.future.tailormade.command.order.GetUserOrderByIdCommand;
+import com.future.tailormade.command.order.GetUserOrdersCommand;
 import com.future.tailormade.command.order.RejectTailorOrderCommand;
 import com.future.tailormade.constants.ApiPath;
 import com.future.tailormade.model.entity.order.Order;
+import com.future.tailormade.model.enums.OrderStatusEnum;
 import com.future.tailormade.payload.request.order.AcceptTailorOrderRequest;
 import com.future.tailormade.payload.request.order.GetTailorOrderByIdRequest;
+import com.future.tailormade.payload.request.order.GetTailorOrdersRequest;
 import com.future.tailormade.payload.request.order.GetUserOrderByIdRequest;
+import com.future.tailormade.payload.request.order.GetUserOrdersRequest;
 import com.future.tailormade.payload.request.order.RejectTailorOrderRequest;
+import com.future.tailormade.payload.response.order.GetOrdersResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
 
     @Autowired
     private CommandExecutor commandExecutor;
+
+    @GetMapping(ApiPath.USERS_ID_ORDERS)
+    public Mono<Response<List<GetOrdersResponse>>> getUserOrders(
+            @PathVariable("userId") String userId,
+            @RequestParam("page") Integer page,
+            @RequestParam("itemPerPage") Integer itemPerPage
+    ) {
+        GetUserOrdersRequest request = GetUserOrdersRequest.builder()
+                .userId(userId)
+                .page(page)
+                .itemPerPage(itemPerPage)
+                .build();
+        return commandExecutor.execute(GetUserOrdersCommand.class, request)
+                .map(getUserOrdersResponse -> com.future.tailormade.payload.response.base.helper
+                        .ResponseHelper.ok(
+                                getUserOrdersResponse.getData(),
+                                page,
+                                itemPerPage,
+                                getUserOrdersResponse.getTotalItem()
+                        )
+                )
+                .subscribeOn(Schedulers.elastic());
+    }
+
+    @GetMapping(ApiPath.TAILORS_ID_ORDERS)
+    public Mono<Response<List<GetOrdersResponse>>> getTailorOrders(
+            @PathVariable("tailorId") String tailorId,
+            @RequestParam("status") OrderStatusEnum status,
+            @RequestParam("page") Integer page,
+            @RequestParam("itemPerPage") Integer itemPerPage
+    ) {
+        GetTailorOrdersRequest request = GetTailorOrdersRequest.builder()
+                .tailorId(tailorId)
+                .status(status)
+                .page(page)
+                .itemPerPage(itemPerPage)
+                .build();
+        return commandExecutor.execute(GetTailorOrdersCommand.class, request)
+                .map(getTailorOrdersResponse -> com.future.tailormade.payload.response.base.helper
+                        .ResponseHelper.ok(
+                                getTailorOrdersResponse.getData(),
+                                page,
+                                itemPerPage,
+                                getTailorOrdersResponse.getTotalItem()
+                        )
+                )
+                .subscribeOn(Schedulers.elastic());
+    }
 
     @GetMapping(ApiPath.USERS_ID_ORDERS_ID)
     public Mono<Response<Order>> getUserOrderById(
@@ -44,8 +101,8 @@ public class OrderController {
 
     @GetMapping(ApiPath.TAILORS_ID_ORDERS_ID)
     public Mono<Response<Order>> getTailorOrderById(
-                @PathVariable("tailorId") String tailorId,
-                @PathVariable("id") String id
+            @PathVariable("tailorId") String tailorId,
+            @PathVariable("id") String id
     ) {
         GetTailorOrderByIdRequest request = GetTailorOrderByIdRequest.builder()
                 .id(id)
