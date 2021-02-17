38e651f64f2ac8cc1bec7ea18f19be0dfc6336b6
syntialai <53588149+syntialai@users.noreply.github.com>
2021-02-18 00:28:39 +0700

    DESIGN-UNIT-TEST: Add unit test for get designs

--- app/src/test/java/com/future/tailormade/BaseTest.java
+++ app/src/test/java/com/future/tailormade/BaseTest.java
@@ -2,12 +2,25 @@ package com.future.tailormade;
 
 import com.future.tailormade.model.enums.GenderEnum;
 import com.future.tailormade.model.enums.RoleEnum;
+import com.future.tailormade.payload.response.base.BasePagingResponse;
 import org.junit.After;
 import org.junit.Before;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Collections;
 import java.util.UUID;
 
 public abstract class BaseTest {
+
+    protected static final Integer PAGE = 0;
+    protected static final Integer ITEM_PER_PAGE = 20;
+    protected static final Integer TOTAL_ITEM = 1;
+    protected static final Integer TOTAL_PAGE = 1;
+    protected static final Long ITEM_COUNT = 1L;
+    protected static final Long ITEM_EMPTY_COUNT = 0L;
 
     protected static final String USER_ID = UUID.randomUUID().toString();
     protected static final String USER_NAME = "USER_NAME";
@@ -17,9 +30,33 @@ public abstract class BaseTest {
 
     protected static final RoleEnum TAILOR_ROLE = RoleEnum.ROLE_TAILOR;
 
+    protected static final String DESIGN_ID = "DESIGN ID";
+    protected static final String DESIGN_TITLE = "DESIGN TITLE";
+
     @Before
     public abstract void setUp();
 
     @After
     public abstract void tearDown();
+
+    @Captor
+    protected ArgumentCaptor<Pageable> pageableCaptor;
+
+    public Pageable getPageable() {
+        return PageRequest.of(PAGE, ITEM_PER_PAGE);
+    }
+
+    public <T> BasePagingResponse<T> createPaging(T data) {
+        return BasePagingResponse.<T>builder()
+                .totalItem(TOTAL_ITEM)
+                .data(Collections.singletonList(data))
+                .build();
+    }
+
+    public <T> BasePagingResponse<T> createPaging() {
+        return BasePagingResponse.<T>builder()
+                .totalItem(ITEM_EMPTY_COUNT.intValue())
+                .data(Collections.emptyList())
+                .build();
+    }
 }
